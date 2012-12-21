0908a71c784deadd14ca68a261c782aac656b85b
Michael Hendricks <michael@ndrix.org>
2012-12-21 16:41:39 -0700

    Better pack title

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(mavis).
-version('0.0.1').
-title('Optional type declarations using attributed variables').
+version('0.0.2').
+title('Optional type declarations').
 keywords([types]).
 author( 'Michael Hendricks', 'michael@ndrix.org' ).
 packager( 'Michael Hendricks', 'michael@ndrix.org' ).
 maintainer( 'Michael Hendricks', 'michael@ndrix.org' ).
 home('https://github.com/mndrix/mavis').
-download('https://github.com/mndrix/mavis/archive/0.0.1.zip').
+download('https://github.com/mndrix/mavis/archive/0.0.2.zip').
