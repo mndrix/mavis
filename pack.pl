38c5b52a6153e5927ef962373fdd3d419f670c39
Michael Hendricks <michael@ndrix.org>
2012-12-21 17:28:51 -0700

    Bump version to 0.0.5

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(mavis).
-version('0.0.4').
+version('0.0.5').
 title('Optional type declarations').
 keywords([types]).
 author( 'Michael Hendricks', 'michael@ndrix.org' ).
 packager( 'Michael Hendricks', 'michael@ndrix.org' ).
 maintainer( 'Michael Hendricks', 'michael@ndrix.org' ).
 home('https://github.com/mndrix/mavis').
-download('http://commondatastorage.googleapis.com/ndrix/mavis-0.0.4.zip').
+download('http://commondatastorage.googleapis.com/ndrix/mavis-0.0.5.zip').
